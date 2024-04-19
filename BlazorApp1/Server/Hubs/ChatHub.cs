de163b604a7aa5310a7c3c0c6fa69c0cdf4af4e7
kyw <rladyddn258@gmail.com>
2024-04-19 16:14:33 +0900

    temp

--- BlazorApp1/Server/Hubs/ChatHub.cs
+++ BlazorApp1/Server/Hubs/ChatHub.cs
@@ -5,23 +5,29 @@ namespace BlazorApp1.Server.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task JoinRoomAsync(string roomName)
+        public async Task JoinRoomAsync(string userid, string roomName)
         {
+            // DB 검증로직 
+            //          - 필요없을수도
             await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
             
             await Clients.Group(roomName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} has joined the room {roomName}");
         }
 
-        public async Task RemoveRoomAsync(string roomName)
+        public async Task RemoveRoomAsync(string userid, string roomName)
         {
+            // DB 검증로직
+            //          - 필요없을수도
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
             await Clients.Group(roomName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} has left the group {roomName}.");
         }
 
         public async Task SendMessageAsync(string message, string roomName)
         {
-            await Clients.Group(roomName).SendAsync("ReceiveMessage", $"{Context.ConnectionId}: {message}");
+            //await Clients.Group(roomName).SendAsync("ReceiveMessage", $"{Context.ConnectionId}: {message}");
+            await Clients.Group(roomName).SendAsync("ReceiveMessage", $"{message}");
         }
+
     }
     
 }
